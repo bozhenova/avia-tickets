5b93f68c72bc09c1d7890ecafb29da6007117977
Margo <margo.bozhenova@gmail.com>
2020-05-26 03:23:41 +0300

    Add layout and styles

--- src/js/plugins/materialize.js
+++ src/js/plugins/materialize.js
@@ -7,3 +7,23 @@ M.FormSelect.init(select);
 export function getSelectInstance(elem) {
   return M.FormSelect.getInstance(elem);
 }
+
+const autocomplete = document.querySelectorAll('.autocomplete');
+M.Autocomplete.init(autocomplete, {
+  data: {
+    Apple: null,
+    Microsoft: null,
+    Google: 'https://placehold.it/250x250'
+  }
+});
+
+export function getAutocompleteInstance(elem) {
+  return M.Autocomplete.getInstance(elem);
+}
+
+const datepickers = document.querySelectorAll('.datepicker');
+M.Datepicker.init(datepickers, { showClearBtn: true });
+
+export function getDatePickerInstance(elem) {
+  return M.Datepicker.getInstance(elem);
+}
